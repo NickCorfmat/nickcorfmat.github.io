74719bd6fe62bfbd32f2860b8061b6ccf4cc0c04
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-16 21:45:34 -0800

    footer update

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom';
+import github_icon from '../../../public/Assets/icons/github.svg'
+import linkedin_icon from '../../../public/Assets/icons/linkedin.svg'
 
 const Footer = () => {
   return (
@@ -12,10 +14,12 @@ const Footer = () => {
           <li><Link to="/contact" className="nav-link">CONTACT</Link></li>
         </ul>
         <div className="footer-social-icons">
-          <a href="https://facebook.com" target="_blank" className="social-icon">Facebook</a>
-          <a href="https://twitter.com" target="_blank" className="social-icon">Twitter</a>
-          <a href="https://instagram.com" target="_blank" className="social-icon">Instagram</a>
-          <a href="https://linkedin.com" target="_blank" className="social-icon">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/nicolascorfmat/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin_icon} className="linkedin-icon" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/NickCorfmat" target="_blank" rel="noopener noreferrer">
+            <img src={github_icon} className="github-icon" alt="LinkedIn" />
+          </a>
         </div>
     </div>
   )
