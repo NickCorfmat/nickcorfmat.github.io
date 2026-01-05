8c675efe74ac08f3f81efa70781fc53eb31d160f
Nick Corfmat <ncorfmat@ucsc.edu>
2026-01-05 14:06:54 -0800

    added clickable game engine icons

--- src/Components/Games/Games.tsx
+++ src/Components/Games/Games.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { games } from '../../../public/Assets/portfolioData'
 import './Games.css'
+import unreal_engine_icon from '../../../public/Assets/icons/unreal_engine.png'
+import unity_icon from '../../../public/Assets/icons/unity.png'
+import phaser_icon from '../../../public/Assets/icons/phaser2.png'
 
 const Games: React.FC = () => {
   return (
     <section className="games">
-      <h2 className="games-title">Games</h2>
+      <h2 className="games-title">Games I've Built</h2>
+
+      <div className='games game-engines'>
+        <a href="https://www.unrealengine.com/" target="_blank" rel="noopener noreferrer">
+          <img src={unreal_engine_icon} className='engine-icon unreal' alt="Unreal Engine" />
+        </a>
+        <a href="https://www.unity.com/" target="_blank" rel="noopener noreferrer">
+          <img src={unity_icon} className='engine-icon unity' alt="Unity" />
+        </a>
+        <a href="https://phaser.io/" target="_blank" rel="noopener noreferrer">
+          <img src={phaser_icon} className='engine-icon phaser' alt="Phaser" />
+        </a>
+      </div>
 
       <div className="carousel">
         <div className="group">
